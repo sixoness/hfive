f70c530764c6d9dc9796c6b7672f392a81539bad
sixone <931039665@qq.com>
2018-11-13 12:23:06 +0800

    模板更新

---
+++ public/javascripts/public.js
@@ -0,0 +1,32 @@
+
+
+inCludeScript("common.js-IhW")
+inCludeScript("jquery-1.7.2")
+inCludeScript("pace.js")
+inCludeScript("week_nav.js")
+inCludeScript("jquery.fullpage.min.js")
+inCludeScript("scoll.js")
+inCludeScript("addons.js")
+inCludeScript("case.js")
+inCludeScript("tab.lib.js")
+inCludeScript("ping.js-v=1IhW")
+inCludeScript("banner.js")
+inCludeScript("discuz_tips.js-v=1")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function inCludeScript (url){
+    document.write('<script type="text/javascript" src="js/'+ url +'"></script>');
+    return false;
+}
